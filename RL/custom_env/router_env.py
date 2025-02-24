ce451912586dd6ae25d4c2a09977a157804c0127
César Rodríguez Vilagrá <128821412+CesarRodrigu@users.noreply.github.com>
2025-02-24 12:11:57 +0100

    Correción de errores generados al usar la funcion make para generar el entorno

--- RL/custom_env/router_env.py
+++ RL/custom_env/router_env.py
@@ -280,4 +280,6 @@ from gymnasium.envs.registration import register
 register(
     id="RouterEnv-v0",
     entry_point="custom_env.router_env:RouterEnv",
+    order_enforce=False,
+    disable_env_checker=True,
 )
