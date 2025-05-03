f4a230c9e4154992f5a11a3bd3f996dfe4333858
César Rodríguez Vilagrá <128821412+CesarRodrigu@users.noreply.github.com>
2025-05-03 15:36:32 +0200

    Eliminación del anterior backend en flask

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv(verbose=True)
-
-HOST: str = os.getenv('APP_HOST', '0.0.0.0')
-PORT: int = int(os.getenv('APP_PORT', 8000))
-DEBUG: bool = bool(os.getenv('APP_DEBUG', False))
--- app/src/routes.py
+++
@@ -1,9 +0,0 @@
-
-from flask import Blueprint, render_template,url_for
-
-bp = Blueprint('main', __name__)
-
-
-@bp.route('/')
-def home() -> str:
-    return render_template('app_main.html')
